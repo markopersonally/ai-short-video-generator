b0014fd4ae387f2f3233d26d61969d462d7cf9ac
mkpage1337 <mrku1337@gmail.com>
2024-10-08 12:50:03 +0200

    new/user

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Test</h2>
       <Button variant="destructive">Test</Button>
+
+      <UserButton />
     </div>
   );
 }
